d848a3d66d08accde4937429c4a4d0f3e1e79298
Audrey <a.comlancataria@epsi.fr>
2019-12-08 21:34:41 +0100

    Resume data passed to props

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 export default class Resume extends Component {
   render() {
+    let resumeData = this.props.resumeData;
+
     return (
       <React.Fragment>
         <section id="resume">
@@ -9,39 +11,51 @@ export default class Resume extends Component {
           <div className="row education">
             <div className="three columns header-col">
               <h1>
-                <span>Education</span>
+                <span>{resumeData.resume.education}</span>
               </h1>
             </div>
             <div className="nine columns main-col">
               <div className="row item">
                 <div className="twelve columns">
-                  <h3>University of Life</h3>
+                  <h3>{resumeData.resume.epsi.schoolName}</h3>
                   <p className="info">
-                    Master in Graphic Design <span>•</span> <em className="date">April 2007</em>
+                    {resumeData.resume.epsi.degree} <span>•</span>{' '}
+                    <em className="date">{resumeData.resume.epsi.date}</em>
                   </p>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget
-                    dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,
-                    nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,
-                    sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec,
-                    vulputate eget, arcu. Nullam dictum felis eu pede mollis pretium.
-                  </p>
+                  <p>{resumeData.resume.epsi.description}</p>
                 </div>
               </div>{' '}
               {/* item end */}
               <div className="row item">
                 <div className="twelve columns">
-                  <h3>School of Cool Designers</h3>
+                  <h3>{resumeData.resume.tivoli.schoolName}</h3>
                   <p className="info">
-                    B.A. Degree in Graphic Design <span>•</span> <em className="date">March 2003</em>
+                    {resumeData.resume.tivoli.degree} <span>•</span>{' '}
+                    <em className="date">{resumeData.resume.tivoli.date}</em>
                   </p>
-                  <p>
-                    This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
-                    Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis
-                    sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi
-                    accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non mauris
-                    vitae erat
+                  <p>{resumeData.resume.tivoli.description}</p>
+                </div>
+              </div>{' '}
+              {/* item end */}
+              <div className="row item">
+                <div className="twelve columns">
+                  <h3>{resumeData.resume.uBordeaux.schoolName}</h3>
+                  <p className="info">
+                    {resumeData.resume.uBordeaux.degree} <span>•</span>{' '}
+                    <em className="date">{resumeData.resume.uBordeaux.date}</em>
                   </p>
+                  <p>{resumeData.resume.uBordeaux.description}</p>
+                </div>
+              </div>{' '}
+              {/* item end */}
+              <div className="row item">
+                <div className="twelve columns">
+                  <h3>{resumeData.resume.descartes.schoolName}</h3>
+                  <p className="info">
+                    {resumeData.resume.descartes.degree} <span>•</span>{' '}
+                    <em className="date">{resumeData.resume.descartes.date}</em>
+                  </p>
+                  <p>{resumeData.resume.descartes.description}</p>
                 </div>
               </div>{' '}
               {/* item end */}
@@ -54,39 +68,51 @@ export default class Resume extends Component {
           <div className="row work">
             <div className="three columns header-col">
               <h1>
-                <span>Work</span>
+                <span>{resumeData.resume.work}</span>
               </h1>
             </div>
             <div className="nine columns main-col">
               <div className="row item">
                 <div className="twelve columns">
-                  <h3>Awesome Design Studio</h3>
+                  <h3>{resumeData.resume.cdiscount2.companyName}</h3>
                   <p className="info">
-                    Senior UX Designer <span>•</span> <em className="date">March 2010 - Present</em>
+                    {resumeData.resume.cdiscount2.job} <span>•</span>{' '}
+                    <em className="date">{resumeData.resume.cdiscount2.date}</em>
                   </p>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget
-                    dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,
-                    nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,
-                    sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec,
-                    vulputate eget, arcu. Nullam dictum felis eu pede mollis pretium.
-                  </p>
+                  <p>{resumeData.resume.cdiscount2.description}</p>
                 </div>
               </div>{' '}
               {/* item end */}
               <div className="row item">
                 <div className="twelve columns">
-                  <h3>Super Cool Studio</h3>
+                  <h3>{resumeData.resume.cdiscount1.companyName}</h3>
                   <p className="info">
-                    UX Designer <span>•</span> <em className="date">March 2007 - February 2010</em>
+                    {resumeData.resume.cdiscount1.job} <span>•</span>{' '}
+                    <em className="date">{resumeData.resume.cdiscount1.date}</em>
                   </p>
-                  <p>
-                    This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
-                    Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis
-                    sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi
-                    accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non mauris
-                    vitae erat
+                  <p>{resumeData.resume.cdiscount1.description}</p>
+                </div>
+              </div>{' '}
+              {/* item end */}
+              <div className="row item">
+                <div className="twelve columns">
+                  <h3>{resumeData.resume.reed.companyName}</h3>
+                  <p className="info">
+                    {resumeData.resume.reed.job} <span>•</span>{' '}
+                    <em className="date">{resumeData.resume.reed.date}</em>
                   </p>
+                  <p>{resumeData.resume.reed.description}</p>
+                </div>
+              </div>{' '}
+              {/* item end */}
+              <div className="row item">
+                <div className="twelve columns">
+                  <h3>{resumeData.resume.tef.companyName}</h3>
+                  <p className="info">
+                    {resumeData.resume.tef.job} <span>•</span>{' '}
+                    <em className="date">{resumeData.resume.tef.date}</em>
+                  </p>
+                  <p>{resumeData.resume.tef.description}</p>
                 </div>
               </div>{' '}
               {/* item end */}
@@ -99,29 +125,29 @@ export default class Resume extends Component {
           <div className="row skill">
             <div className="three columns header-col">
               <h1>
-                <span>Skills</span>
+                <span>{resumeData.resume.skills}</span>
               </h1>
             </div>
             <div className="nine columns main-col">
-              <p>
+              {/* <p>
                 Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
                 laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto
                 beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
                 odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
-              </p>
+              </p> */}
               <div className="bars">
                 <ul className="skills">
                   <li>
-                    <span className="bar-expand photoshop" />
-                    <em>Photoshop</em>
+                    <span className="bar-expand csharp" />
+                    <em>C#</em>
                   </li>
                   <li>
-                    <span className="bar-expand illustrator" />
-                    <em>Illustrator</em>
+                    <span className="bar-expand sql" />
+                    <em>SQL</em>
                   </li>
                   <li>
-                    <span className="bar-expand wordpress" />
-                    <em>Wordpress</em>
+                    <span className="bar-expand javascript" />
+                    <em>Javascript</em>
                   </li>
                   <li>
                     <span className="bar-expand css" />
@@ -132,8 +158,8 @@ export default class Resume extends Component {
                     <em>HTML5</em>
                   </li>
                   <li>
-                    <span className="bar-expand jquery" />
-                    <em>jQuery</em>
+                    <span className="bar-expand agile" />
+                    <em>Agile</em>
                   </li>
                 </ul>
               </div>
