451d42c578e988cac372062890093a06d1114314
Audrey Desktop <48823375+Koulikana@users.noreply.github.com>
2020-01-12 15:33:00 +0100

    Updates

--- precache-manifest.7f27b500e55ea6da10520239b4e2e551.js
+++ precache-manifest.7f27b500e55ea6da10520239b4e2e551.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a9ff7433e0879eb6005d83a82bda7342",
+    "revision": "13553bbbf5a1f41fae757c13a747409d",
     "url": "/resume/index.html"
   },
   {
-    "revision": "3a39899075e4e7d54677",
+    "revision": "8a76e9504bd30c1b69d0",
     "url": "/resume/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "40a317bcf0e2a068030b",
-    "url": "/resume/static/js/2.bd14c8d3.chunk.js"
+    "revision": "7818baa436cdcc8e8d16",
+    "url": "/resume/static/js/2.a0c9c9e1.chunk.js"
   },
   {
     "revision": "d705cb622423d72c5defbf368ca70dcc",
-    "url": "/resume/static/js/2.bd14c8d3.chunk.js.LICENSE"
+    "url": "/resume/static/js/2.a0c9c9e1.chunk.js.LICENSE"
   },
   {
-    "revision": "3a39899075e4e7d54677",
-    "url": "/resume/static/js/main.a572a201.chunk.js"
+    "revision": "8a76e9504bd30c1b69d0",
+    "url": "/resume/static/js/main.c545ef72.chunk.js"
   },
   {
     "revision": "754cb4722296976aff30",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/resume/precache-manifest.d62c321045a8d0a603a74efe609ccc6e.js"
+  "/resume/precache-manifest.7f27b500e55ea6da10520239b4e2e551.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/resume/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
